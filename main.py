3c2f180142b49d7eeb1e8a923c4d661040e24759
Matthew Philibert-Jones <m.philibert.jones@gmail.com>
2023-02-21 14:36:23 -0500

    L'ajout du fichier music au tp2 et début de l'ajout de music dans l'écran menu

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 import pygame, sys
+from pygame import mixer
 from pygame.locals import *
 from lib import enemies, heroes, items
 from grid import *
 import random
 from key_events import KeyEvents
 import math
+
+# SOUND
+#mixer.init()
+
+# LOAD AUDIO FILE
+#mixer.music.load('Overworld BGM.mp3')
+#mixer.music.play
 
 # INSTANCES OF GAME OBJECTS
 PLAYER = heroes.LINK()
