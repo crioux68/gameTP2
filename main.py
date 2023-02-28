d03293b159eb6a04157615519461c84326126c4c
Matthew Philibert-Jones <m.philibert.jones@gmail.com>
2023-02-28 14:13:31 -0500

    Import ajouté pour la music

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import pygame, sys
+from pygame import mixer
 from pygame.locals import *
 from lib import enemies, heroes, items
 from grid import *
 import random
 from key_events import KeyEvents
 import math
+
+mixer.init()
+
 
 # CHECK IF THE TILE IS AN OBSTACLE
 def CheckIfObstacles(posTileX, posTileY):
